ade83ceacdd4bd7b3c6188440e0da8d3478e6667
Venkey0467 <venkataramana3094@gmail.com>
2023-10-16 12:54:14 +0530

    SecondCommit

--- src/test/java/EclipseToHub/Test1.java
+++ src/test/java/EclipseToHub/Test1.java
@@ -5,6 +5,10 @@ public class Test1 {
 	public static void printName() {
 		System.out.println("Yuvan Surya Sai");
 	}
+	
+	public static void printRole() {
+		System.out.println("Developer");
+	}
 
 	public static void main(String[] args) {
 		printName();
