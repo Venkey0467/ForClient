fce720e599357737ca6b47c2aacae31ab86ace00
Venkey0467 <venkataramana3094@gmail.com>
2023-10-16 13:02:26 +0530

    FourthCommit

---
+++ src/test/java/EclipseToHub/Test3.java
@@ -0,0 +1,12 @@
+package EclipseToHub;
+
+public class Test3 {
+
+	public static void printHabits() {
+		System.out.println("Sleeping");
+	}
+	
+	public static void main(String[] args) {
+		printHabits();
+	}
+}
