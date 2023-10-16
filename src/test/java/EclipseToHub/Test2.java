5cd59363ee6adec8dcea450c871dd65eb476853a
Venkey0467 <venkataramana3094@gmail.com>
2023-10-16 13:14:01 +0530

    SubCommit

---
+++ src/test/java/EclipseToHub/Test2.java
@@ -0,0 +1,12 @@
+package EclipseToHub;
+
+public class Test2 {
+
+	public static void printPhoneNumber() {
+		System.out.println("8500888737");
+	}
+	
+	public static void main(String[] args) {
+		printPhoneNumber();
+	}
+}
